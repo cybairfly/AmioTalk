442f3dec85703360faed6c41faad230caf96c67f
Tobey <cyberfly@email.cz>
2018-02-15 16:19:26 +0100

    parser

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
+app.use(parser.json());
 
 require('dotenv').config();
 
